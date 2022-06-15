069d06ac08b451e8a28c10f6e62c891ec74fe6c5
Gautam Jain <astrogeek77@gmail.com>
2022-06-15 21:10:29 +0530

    Added Rotate image (2.6)

---
+++ Day 2/48-Rotate-Image.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/rotate-image/
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &m)
+    {
+        //   take transpose of the matrix
+        int n = m.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                swap(m[i][j], m[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            reverse(m[i].begin(), m[i].end());
+        }
+    }
+};
